c09cc46b9bbf1806e4fab5c2e817ad5375589dc4
mrbmadrid <mrbmadrid@gmail.com>
2018-04-07 12:51:09 -0700

    Add form validation.

--- server.py
+++ server.py
@@ -1,15 +1,25 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, session, flash
 app = Flask(__name__)
-
+app.secret_key = '12AB3FEE4589'
 @app.route('/')
 def index():
 	return render_template('index.html')
 
+@app.route('/results')
+def results():
+	return render_template('results.html')
+
 @app.route('/submission', methods=['POST'])
 def submission():
-	name=request.form['name']
-	location=request.form['location']
-	favorite=request.form['favorite_language']
-	comment=request.form['comment']
-	return render_template('results.html', name=name, location=location, favorite=favorite, comment=comment)
+	if len(request.form['name']) == 0:
+		flash("Name field must not be empty.")
+		return redirect('/')
+	session['name']=request.form['name']
+	session['location']=request.form['location']
+	session['favorite']=request.form['favorite_language']
+	if len(request.form['comment']) == 0:
+		flash("Comment field must not be empty.")
+		return redirect('/')
+	session['comment']=request.form['comment']
+	return redirect('/results')
 app.run(debug=True)
